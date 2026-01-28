687dba527b608049efea77882046e5be8d7a0f7c
Snowy <97126807+Snowy7@users.noreply.github.com>
2026-01-28 13:19:47 +0300

    posthog

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { PostHogProvider } from 'posthog-js/react'
 import './globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === 'development',
+      }}
+    >
+      <RouterProvider router={router} />
+    </PostHogProvider>
   </React.StrictMode>
 )
